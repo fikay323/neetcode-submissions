6c2b09e4d62bb8930a2e237342199d20a9e8163e
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2026-07-03 14:06:07 +0100

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        var left = 0;
+        var right = s.Length - 1;
+        while(left < right) {
+            while(left < right && !char.IsLetterOrDigit(s[left])) {
+                left++;
+            }
+
+            while(left < right && !char.IsLetterOrDigit(s[right])) {
+                right--;
+            }
+            
+            if(char.ToLower(s[left]) != char.ToLower(s[right])) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
